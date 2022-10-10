0d23d06dfcd58307dee6067597aba9e3fa8d9a55
bukowa <gitbukowa@gmail.com>
2022-10-10 17:51:14 +0200

    scripts/helm-docs.sh: specify source of this file
    Signed-off-by: bukowa <gitbukowa@gmail.com>

--- scripts/helm-docs.sh
+++ scripts/helm-docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## this file is copied from
+## https://github.com/argoproj/argo-helm/blob/20924aced32bf53228876753ac879bef5c5f0c3e/scripts/helm-docs.sh
 ## Reference: https://github.com/norwoodj/helm-docs
 set -eux
 CHART_DIR="$(cd "$(dirname "$0")/.." && pwd)"
