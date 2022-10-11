c558ada8e6d4f44c3b5f7878e3ddd0abd4537238
Buk Bukowski <gitbukowa@gmail.com>
2022-10-11 22:45:06 +0200

    ci: update
    Signed-off-by: Buk Bukowski <gitbukowa@gmail.com>

--- scripts/generate-changelog.sh
+++ scripts/generate-changelog.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 set -eux
 
+echo "Building docker image for git-cliff..."
+
+GIT_CLIFF=$(docker build -q - <<EOF
+  FROM ghcr.io/orhun/git-cliff/git-cliff:sha-0f38960
+  WORKDIR /charts
+  ENTRYPOINT ["git-cliff"]
+EOF
+)
+
 for chart in charts/*; do
-  touch "$chart/CHANGELOG.md"
   echo "Generating CHANGELOG for $chart"
-  git cliff \
-    --include-path "${chart}/*" \
-    --prepend "$chart/CHANGELOG.md" \
-    --tag="$(grep -Po '(?<=^version: )[0-9.a-zA-Z]*' "${chart}/Chart.yaml")" \
-    -l
+  touch "$chart/CHANGELOG.md"
+  docker run --rm \
+  --volume="${PWD}:/charts" \
+  -u "$(id -u)" \
+    \
+    "${GIT_CLIFF}" \
+    \
+      --include-path "${chart}/*" \
+      --prepend "$chart/CHANGELOG.md" \
+      --tag="$(grep -Po '(?<=^version: )[0-9.a-zA-Z]*' "${chart}/Chart.yaml")" \
+      -l \
+      --verbose
 done
+
+
+## GENERATE WITHOUT DOCKER
+
+#CHART_DIR="${PWD}/charts"
+#echo "${CHART_DIR}"
+#
+#for chart in charts/*; do
+#  touch "$chart/CHANGELOG.md"
+#  echo "Generating CHANGELOG for $chart"
+#  git cliff \
+#    --include-path "${chart}/*" \
+#    --prepend "$chart/CHANGELOG.md" \
+#    --tag="$(grep -Po '(?<=^version: )[0-9.a-zA-Z]*' "${chart}/Chart.yaml")" \
+#    -l --verbose
+#done
---
+++ scripts/git-diff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $(git diff --stat) != '' ]]; then
+  echo -e '\033[0;31mGit outdated!\033[0m ❌'
+  git diff --color
+  exit 1
+else
+  echo -e '\033[0;32mGit up to date\033[0m ✔'
+fi
