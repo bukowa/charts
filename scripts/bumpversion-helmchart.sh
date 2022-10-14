78d96490a8a9266a88869a47427bba5e9fa14e7b
Buk Bukowski <gitbukowa@gmail.com>
2022-10-15 00:34:18 +0200

    update: renovate.json
    Signed-off-by: Buk Bukowski <gitbukowa@gmail.com>

--- scripts/bumpversion-helmchart.sh
+++ scripts/bumpversion-helmchart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -eux
 
-BUMP=$(echo "$MESSAGE" | grep -Po '(?<=^chore\(deps\): )(patch|major|minor)')
-./scripts/semver bump "${BUMP}" "$(grep -Po '(?<=^version: )[\x27\x220-9].*' "charts/${CHART}/Chart.yaml")"
+# CHART="http-headers"
+# MESSAGE="chore(deps): patch update quay.io/k8start/http-headers docker tag to v0.1.3"
+export BUMP=$(echo "$MESSAGE" | grep -Po '(?<=^chore\(deps\): )(patch|major|minor)')
+export VERSION=$(./scripts/semver bump "${BUMP}" "$(yq -e ".version" < ./charts/http-headers/Chart.yaml)")
+yq -e -i ".version = \"${VERSION}\"" ./charts/http-headers/Chart.yaml
