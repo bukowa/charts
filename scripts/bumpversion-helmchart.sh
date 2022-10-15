ef8e4f8791e3e6462736a5aa382de6e63ab18ab3
Buk Bukowski <gitbukowa@gmail.com>
2022-10-15 13:53:41 +0200

    update: renovate.json
    Signed-off-by: Buk Bukowski <gitbukowa@gmail.com>

--- scripts/bumpversion-helmchart.sh
+++ scripts/bumpversion-helmchart.sh
@@ -3,9 +3,9 @@ set -eux
 
 # CHART="http-headers"
 # MESSAGE="chore(deps): patch update quay.io/k8start/http-headers docker tag to v0.1.3"
-if [[ -z "$CHART" ]]; then >&2 echo "CHART must be set"; exit 1; fi
-if [[ -z "$BUMP" ]]; then
-  if [[ -z "$MESSAGE" ]]; then >&2 echo "MESSAGE or BUMP must be set!"; exit 1; fi
+if [[ -z "${CHART+x}" ]]; then >&2 echo "CHART must be set"; exit 1; fi
+if [[ -z "${BUMP+x}" ]]; then
+  if [[ -z "${MESSAGE+x}" ]]; then >&2 echo "MESSAGE or BUMP must be set!"; exit 1; fi
   BUMP=$(echo "$MESSAGE" | grep -Po '(?<=^chore\(deps\): )(patch|major|minor)')
 fi
 
