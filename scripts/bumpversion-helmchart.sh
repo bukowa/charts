ec8e7acea004c5210f88a45d0b3348b13080e0b2
Buk Bukowski <gitbukowa@gmail.com>
2022-10-28 16:24:51 +0200

    chore(ci): bump `appVersion` in ci
    Signed-off-by: Buk Bukowski <gitbukowa@gmail.com>

--- scripts/bumpversion-helmchart.sh
+++ scripts/bumpversion-helmchart.sh
@@ -7,6 +7,14 @@ if [[ -z "${CHART+x}" ]]; then >&2 echo "CHART must be set"; exit 1; fi
 if [[ -z "${BUMP+x}" ]]; then
   if [[ -z "${MESSAGE+x}" ]]; then >&2 echo "MESSAGE or BUMP must be set!"; exit 1; fi
   BUMP=$(echo "$MESSAGE" | grep -Po '(?<=^chore\(deps\): )(patch|major|minor)')
+  APP=$(cat "./charts/${CHART}/app")
+fi
+
+APP_MATCH="update ${APP} docker tag to v"
+
+if [[ "${MESSAGE}" =~ ${APP_MATCH} ]]; then
+  APP_VERSION=$(echo "$MESSAGE" | grep -Po '(?<=docker tag to v)(.*)')
+  yq -e -i ".appVersion = \"${APP_VERSION}\"" ./charts/"${CHART}"/Chart.yaml
 fi
 
 VERSION=$(./scripts/semver bump "${BUMP}" "$(yq -e ".version" < ./charts/"${CHART}"/Chart.yaml)")
